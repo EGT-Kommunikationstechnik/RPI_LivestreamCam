93aaf9b41c5836c07f81ebe6f604ee027eb15e20
EGT-Kommunikationstechnik <grebe@uberrider.de>
2021-01-26 01:07:09 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 echo "Instaling LivestreamingCam"
-wget 
+wget "https://raw.githubusercontent.com/EGT-Kommunikationstechnik/RPI_LivestreamCam/main/automatik.py"
+wget "https://raw.githubusercontent.com/EGT-Kommunikationstechnik/RPI_LivestreamCam/main/kill.sh"
+wget "https://raw.githubusercontent.com/EGT-Kommunikationstechnik/RPI_LivestreamCam/main/stream.sh"
+wget "https://raw.githubusercontent.com/EGT-Kommunikationstechnik/RPI_LivestreamCam/main/servo.py"
+echo "URL of the Command Server:"
